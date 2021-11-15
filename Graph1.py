dc47a53e02fc99486f29600841b59845f4b57f82
enrik03 <enriquecasanova10@gmail.com>
2021-11-15 18:49:29 -0400

    G1

--- Graph1.py
+++ Graph1.py
@@ -1,29 +1,66 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import sympy as sp
+
 
 # first graph fun.
-def klk(step,ke,pe,etotal):
+def graph(step, ke, pe, etotal):
+    kke = plt.plot(step, ke, label='Kinetic Energy')
 
+    ppe = plt.plot(step, pe, label='Potential Energy')
 
-    kke = plt.plot(step,ke, label = 'Kinetic Energy')
+    eetotal = plt.plot(step, etotal, label='Total Energy')
 
-    ppe = plt.plot(step,pe, label = 'Potential Energy')
+    #plt.xlabel = ('steps')
+    #plt.ylabel = ('Energy')
 
-    eetotal = plt.plot(step,etotal, label = 'Total Energy')
-
-    plt.xlabel = ('steps') 
-    plt.ylabel = ('Energy')
-
-    #kke.ylabel = ('Kenetic Energy')
-    #ppe.ylabel = ('Potential Energy')
-    #eetotal.ylabel = ('Total')
-
-
-
-
-
-
+    # kke.ylabel = ('Kenetic Energy')
+    # ppe.ylabel = ('Potential Energy')
+    # eetotal.ylabel = ('Total')
+    plt.legend()
     plt.show()
 
-print(klk([0,1,2,3,4,5],[0,2,4,6,8],[8,6,4,2,0],[8,8,8,8,8]))
+
+mylines = []
+
+
+def data(r):
+    with open('log.lammps', 'rt') as myfile:
+        for myline in myfile:
+            # print(myline.split()[1])
+            mylines.append(myline)
+    temp = []
+    kee = []
+    pee = []
+    total = []
+
+    i = 91
+    k = 0
+    while k < 4:
+        while i < r + 92:
+
+            #print(mylines[i].split()[k])
+            if k == 0:
+                temp.append(mylines[i].split()[k])
+                #print(temp)
+            elif k == 1:
+                kee.append(mylines[i].split()[k])
+                #print(kee)
+            elif k == 2:
+                pee.append(mylines[i].split()[k])
+                #print(pee)
+            elif k == 3:
+                total.append(mylines[i].split()[k])
+                #print(total)
+            i = i + 1
+        k = k + 1
+        i = 91
+    #print(total)
+    return graph(temp, kee, pee, total)
+
+
+# run = 100000 , thermo = 100
+def stp(run, thermo):
+    step_t = run / thermo
+    return data(step_t)
+
+
+print(stp(100000, 100))
